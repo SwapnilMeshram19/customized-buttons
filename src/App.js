59e0aa3666578a1f8b69c68b0ee614eaba90f61b
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-07-03 01:21:29 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {Button} from './components/Button';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Buttons Customization</h1>
+      <div>
+      <Button>Default Button</Button>
+      <Button primary>Primary Button</Button>
+      <Button dashed>Dashed Button</Button>
+      <Button text>Text Button</Button>
+      <Button link>Link Button</Button>
+
+      </div>
+     
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,57 @@
+import styled,{css} from "styled-components";
 
-const Button= ()=>{
-    
-}
+const Button = styled.button`
+  border: 1px solid rgb(217,217,217);
+  border-radius:3px;
+  padding: 8px 10px;
+  color: #454545;
+  font-weight: 500;
+  background-color: white;
+  letter-spacing: 0.6px;
+  cursor: pointer;
+  &:hover{
+    background-color: rgb(36,144,254);
+    color: white;
+    transition: color 1s 0s, background-color 1s 0s;
 
-export default Button;
+  }
+
+ 
+  ${props=>props.primary && css`
+    background-color: rgb(36,144,254);
+    color: white;
+
+    &:hover{
+        background-color: white;
+        color: #454545;
+
+
+    }
+  `}
+
+  ${props=>props.dashed && css`
+    border: 1px dashed rgb(217,217,217);
+  `}
+
+  ${props=>props.text && css`
+    border: none;
+    &:hover{
+        border:none;
+        background-color: white;
+        color:black;
+    }
+
+  `}
+  ${props=>props.link && css`
+    border:none;
+    color: rgb(36,144,254); 
+    &:hover{
+        color: #c8005d;
+        background-color: white;
+        border: none;
+    }
+
+  `}
+`;
+
+export { Button };
